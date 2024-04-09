afa67805793a64110d0a075b22c9d4ec04634d51
desmaia <desmaianita@gmail.com>
2024-04-09 23:51:52 +0200

    feature(page): show jobs as a list

--- db/seed.ts
+++ db/seed.ts
@@ -5,6 +5,7 @@ export default async function seed() {
 	await db.insert(Host).values([
 		{ name: "Job1", experience: 'pick up olives', email: "job1@whatever" },
 		{ name: "Job2", experience: 'pick up cherries', email: "job2@whatever"},
+		{ name: "Job3", experience: 'pick up cherries', email: "job2@whatever"}
 	  ])
 	await db.insert(Message).values([
 		{ from: "abc@blabla", to: 'Job1', content: "I want to apply" },
