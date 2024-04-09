e30d94229b4eb87a3a2f2ab991bdf5a35c91b22a
desmaia <desmaianita@gmail.com>
2024-04-09 23:28:33 +0200

    fix(db): Seed Host

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,13 @@
-import { db, Message } from 'astro:db';
+import { db, Message, Host } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
+	await db.insert(Host).values([
+		{ name: "Job1", experience: 'pick up olives', email: "job1@whatever" },
+		{ name: "Job2", experience: 'pick up cherries', email: "job2@whatever"},
+	  ])
 	await db.insert(Message).values([
-		{ from: "abc@blabla", to: 'Job1' },
-		{ from: "xyz@blublu", to: 'Job2'},
+		{ from: "abc@blabla", to: 'Job1', content: "I want to apply" },
+		{ from: "xyz@blublu", to: 'Job2', content: "I also want to apply"},
 	  ])
 }
